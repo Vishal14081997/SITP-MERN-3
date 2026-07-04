bf67f0df3f9eb4470f6a3fdcd6fc0a710aabbc91
Vishal14081997 <1408vishalsingh@gmail.com>
2026-07-04 19:10:06 +0530

    js datatypes

---
+++ js/index.js
@@ -0,0 +1,75 @@
+console.log("hello vishal");
+
+/* multi line comment */
+
+// let , var , const
+
+
+// var - globle scop - redeclare✅ , update✅
+//let - block scop -   redeclare❎ ,update✅
+// const - block scop - redeclare❎ , update❎
+
+// ------------ var ------------
+// var a = 10;
+// console.log(a);
+
+// var a = 20;
+// console.log(a);
+// a = 30;
+// console.log(a);
+
+// ------------ let ------------
+// let a = 33;
+// console.log(a);
+// a = 44;
+// console.log(a);
+
+// --------- const ----------
+// const a = 55;
+// console.log(a);
+
+
+// --------- data types--------
+
+// 1) premtive types
+// 2) non premtive types
+
+// *********premtive types **********
+// a. number(10)
+// b . string("vishal")
+// c . boolean(true)
+// d . null
+// e . undefiend
+// f . symbol
+// g . bigint
+
+// *********non premtive types  **********
+// a) Array  b) object 
+
+// let arr = [1 ,2 ,3 ,4, "vishal" ,true]
+
+// const obj = {
+//     // key:value
+//     age:34,
+//     name:"vishal"
+// }
+// console.log(obj["age"]);
+
+// --------- naming convension-----
+
+// camel case -
+// const myName = "vishal"
+
+// snake case -
+// const my_name = "vishal"
+
+let name  = "vishal"
+console.log(typeof name);
+document.write(typeof name , name)
+
+
+
+
+
+
+
