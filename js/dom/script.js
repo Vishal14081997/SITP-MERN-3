a43174da0328f6d438eb766d027d51d0832c5549
Vishal14081997 <1408vishalsingh@gmail.com>
2026-07-14 19:04:51 +0530

    dom

---
+++ js/dom/script.js
@@ -0,0 +1,16 @@
+
+// document.getElementById("heading").innerHTML = "hy"
+document.getElementById("box").innerHTML = "<h1>main box</h1>"
+
+const handleClick = () => {
+    document.getElementById("heading").innerText = "vishal" 
+    document.getElementById("heading").style.color = "white"
+    document.getElementById("heading").style.background = "red"
+    document.getElementById("heading").style.fontSize = "24px"
+    document.getElementById("heading").style.padding = "10px"
+    document.getElementById("heading").style.display = "inline-block"
+}
+
+
+
+
