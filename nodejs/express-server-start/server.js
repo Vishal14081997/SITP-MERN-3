a7595c9efaa02dc010b6d25eda34996f90caeebe
Vishal14081997 <1408vishalsingh@gmail.com>
2026-07-25 07:32:44 +0530

    loops

---
+++ nodejs/express-server-start/server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const server = express()
+server.get("/",(req,res)=>{
+    // console.log(req.headers); 
+    console.log("hello world");
+    res.send("hello vishal") 
+})
+server.post("/" ,(req,res)=>{
+    console.log("post api");
+    res.send("post api hit")
+})
+server.put("/" , (req,res)=>{
+    console.log("put api");
+    res.send("put api hit")
+    
+})
+server.delete("/studentDelete" , (req,res)=>{
+    console.log("studentDelete api");
+    res.send("studentDelete success") 
+})
+server.listen(3000 ,()=>{
+    console.log("server start 3000");
+})
