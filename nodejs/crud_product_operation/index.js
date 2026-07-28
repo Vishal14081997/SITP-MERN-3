58131707f9336766b40e25f239f282f4018dda55
Vishal14081997 <1408vishalsingh@gmail.com>
2026-07-28 17:41:42 +0530

    product crud

--- nodejs/crud_product_operation/index.js
+++ nodejs/crud_product_operation/index.js
@@ -85,20 +85,71 @@ const Product = mongoose.model("products", productSchema);
 server.post("/createProduct", async (req, res) => {
     try {
         const { productName, productPrice, stock, desc } = req.body;
-
         const items = await Product.create({
-            productName, 
-            productPrice, 
-            stock, 
+            productName,
+            productPrice,
+            stock,
             desc
         });
-
         res.send(items);
+
     } catch (error) {
-        console.log("error message");
+        console.log("createProduct", error.message);
     }
 });
+
+server.get("/getAllProduct", async (req, res) => {
+    try {
+        const getProducts = await Product.find()
+        res.status(200).json({
+            message: "get all product",
+            data: getProducts
+        })
+    } catch (error) {
+        console.log("getAllProduct", error.message);
+    }
+})
+server.get("/getProduct/:id", async (req, res) => {
+    try {
+        const productId = req.params.id
+        console.log(productId);
+
+        const getPoduct = await Product.findById(productId)
+        console.log(getPoduct);
+        res.status(200).json({
+            message: "get product",
+            data: getPoduct
+        })
+    } catch (error) {
+        console.log("getProduct", error.message);
+    }
+})
+
+server.put("/updateProduct/:id", async (req, res) => {
+    try {
+        const productId = req.params.id
+        // console.log(productId);
+        const product = await Product.findByIdAndUpdate(productId, req.body, { new: true })
+        res.status(200).json({
+            message: "product update",
+            data: product
+        })
+    } catch (error) {
+        console.log("updateProduct", error.message);
+    }
+})
 
 server.listen(3000, () => {
     console.log("server start port 3000");
 });
+
+
+
+// 200 - success
+// 201 - create
+// 400 - BAD_REQUEST
+// 401 - Unauthorized
+// 403 - Forbidden
+// 404 - not found
+// 500 - Internal Server Error
+
